b166b4df4e0ed63ef31db848833ccd1a321f71ba
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-11-29 23:05:49 -0300

    Update admin.ts

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,15 +1,17 @@
 import { auth } from "@clerk/nextjs/server";
 
-const adminIds = [
+const allowedIds = [
     "user_2onwE7iHoPdJeHRGX9dqbt87N0L", // IDs dos administradores
 ];
 
-export const isAdmin = () => {
-    const userId = auth();  // Obtém o ID do usuário
+export const isAdmin = async () => {
+    const  userId = await auth();
+    const userIdString = userId.toString();
 
     if (!userId) {
         return false; // Retorna falso se não houver um ID de usuário
     }
 
-    return adminIds.indexOf(userId.toString()) !== -1;  // Verifica se o ID do usuário está na lista de admin
+    return allowedIds.indexOf(userIdString) !== -1;  // Verifica se o ID do usuário está na lista de admin
+    
 }
