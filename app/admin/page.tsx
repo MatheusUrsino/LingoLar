3c225f3ba6eb158859df18d461adeb049e298112
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-11-29 23:06:11 -0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,18 +1,17 @@
 import { isAdmin } from "@/lib/admin";
-import dynamic from "next/dynamic";
+import dynamic from "next/dynamic"
 import { redirect } from "next/navigation";
 
-// Importar o componente client-side (AppClient)
-const App = dynamic(() => import("./app"), { ssr: false });
+const App = dynamic(() => import("./app"));
 
 const AdminPage = async () => {
-  const admin = await isAdmin(); // Verifica se o usuário é admin
-
-  if (!admin) {
-    redirect("/"); // Redireciona para a página principal caso não seja admin
+  if (!isAdmin()) {
+    redirect("/");
   }
 
-  return <App />;
+    return (
+        <App />
+    );
 };
 
-export default AdminPage;
+export default AdminPage
