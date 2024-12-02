fb568a81c568abc4e430365d42632b47f6d3bd5c
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-12-01 21:25:02 -0300

    Update admin.ts

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,19 +1,5 @@
-import { useAuth } from "@clerk/nextjs";
 
-const allowedIds = [
-    "user_2onwE7iHoPdJeHRGX9dqbt87N0L", // IDs dos administradores
-];
 
-// A função é agora um hook, mas será exportada como isAdmin
-export const useIsAdmin = () => {  // Hooks devem começar com "use"
-    const { userId } = useAuth();
-
-    if (!userId) {
-        return false; // Retorna falso se não houver um ID de usuário
-    }
-
-    return allowedIds.indexOf(userId) !== -1; // Verifica se o ID do usuário está na lista de admin
-};
-
-// Exportando com o nome "isAdmin" para manter a compatibilidade
-export const isAdmin = useIsAdmin;
+export const isAdmin = async () => {
+return true;
+}
